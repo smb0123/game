37beda1f8a4187c2d7c891b6bd3eb661b62cfebe
smb0123 <smb5174@naver.com>
2025-10-25 11:57:17 +0900

    choer: react-router-dom 설치 후 경로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { GamePage } from '@/pages/GamePage';
-import './App.css';
+import { GamePlayPage } from "@/pages/GamePlayPage";
+import { GameSettingsPage } from "@/pages/GameSettingsPage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
 
 /**
  * 메인 앱 컴포넌트
+ * React Router를 사용하여 페이지 라우팅을 관리합니다.
  */
 function App() {
   return (
-    <div className="App">
-      <GamePage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* 기본 경로는 게임 설정 페이지로 리다이렉트 */}
+          <Route path="/" element={<GameSettingsPage />} />
+          {/* 게임 플레이 페이지 */}
+          <Route path="/game" element={<GamePlayPage />} />
+          {/* 존재하지 않는 경로는 게임 설정 페이지로 리다이렉트 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
